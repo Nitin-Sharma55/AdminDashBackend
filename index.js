d5e6da0bc67300f7f5c2ade35dbeb2e69f64b99d
Nitin <spidernick234@gmail.com>
2026-04-15 13:18:54 +0530

    adding cors

--- index.js
+++ index.js
@@ -4,9 +4,18 @@ import express from "express";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/auth.js"
 import itemRoutes from "./routes/item.route.js";
+import cors from "cors";
 const app = express();
 
-
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174"
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use("/api/users",authRoutes);
 app.use("/api/user/item",itemRoutes);
